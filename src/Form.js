ad9efe4994fc8ce9e8be6b22801712e010b8e7a1
MishaelRatemo <ratemomishael@gmail.com>
2022-11-09 12:19:57 +0300

    Added fields and state foreach field.-- not recommended esp when one has many fields

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function MyForm() {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+
+    // console.log(firstName,lastName)
+
+    function handleFNChange(event){
+        setFirstName(event.target.value)
+    }
+    function handleLNChange(event){
+        setLastName(event.target.value)
+    }
   return (
     <form>
         <h2>Sign Up</h2>
+        <input 
+            type="text"
+            placeholder='First Name'
+            onChange={handleFNChange}        
+        />
+        <input 
+            type="text"
+            placeholder='Last Name'
+            onChange={handleLNChange}        
+        />
+
     </form>
   )
 }
