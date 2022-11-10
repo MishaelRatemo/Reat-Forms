3cdf143fc7a29aafa39484a1909a1b8898da6675
MishaelRatemo <ratemomishael@gmail.com>
2022-11-10 16:14:40 +0300

    Other form elements       -checkbox

--- src/Form.js
+++ src/Form.js
@@ -1,57 +1,106 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function MyForm() {
-    const [formData, setFormData] = useState({
-        firstName:"", lastName:"", email:"", comment:""
-    })
-    
-    function handleChange(event){
-        setFormData(prevFormData =>{
-            return{
-                ...prevFormData,
-                [event.target.name]:event.target.value
-            }
-        })
-    }
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    comment: "",
+    java: false,
+    csharp:false,
+    python:false,
+    javascript:false
+  });
 
-    console.log(formData)
+  function handleChange(event) {
+    const {name, value,type,checked} = event.target
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        // [event.target.name]: event.target.value,
+        [name]: type==="checkbox" ?checked : value
+      };
+    });
+  }
+
+  console.log(formData);
 
   return (
     <form>
+      <div className="box">
         <h2>Inputs</h2>
-        <input 
-            type="text"
-            placeholder='First Name'
-            onChange={handleChange}
-            name="firstName"
-            value={formData.firstName}        
+        <input
+          type="text"
+          placeholder="First Name"
+          onChange={handleChange}
+          name="firstName"
+          value={formData.firstName}
         />
-        <input 
-            type="text"
-            placeholder='Last Name'
-            onChange={handleChange}
-            name="lastName"
-            value={formData.lastName}        
+        <input
+          type="text"
+          placeholder="Last Name"
+          onChange={handleChange}
+          name="lastName"
+          value={formData.lastName}
         />
-        <input 
-            type="email"
-            placeholder='Email Address'
-            onChange={handleChange}
-            name="email"
-            value={formData.email}        
+        <input
+          type="email"
+          placeholder="Email Address"
+          onChange={handleChange}
+          name="email"
+          value={formData.email}
         />
-        <h2>Textarea</h2>
-        <textarea 
-            type="text"
-            placeholder='Your Comment goes here'
-            onChange={handleChange}
-            name="comment"
-            value={formData.comment}        
-        />
+      </div>
 
+      <div className="box">
+            <h2>Textarea</h2>
+            <textarea
+                type="text"
+                placeholder="Your Comment goes here"
+                onChange={handleChange}
+                name="comment"
+                value={formData.comment}
+            />
+      </div>
+      <div className="box">
+        <h2>CheckBox</h2>
+        <label htmlFor="">Programming languages</label>    
+        <div className="">
+            <input 
+                type="checkbox" 
+                onChange={handleChange}
+                checked={formData.java} 
+                id="java"
+                name="java"/>    
+            <label htmlFor="java">Java</label> 
 
+            <input 
+                type="checkbox" 
+                onChange={handleChange}
+                checked={formData.csharp} 
+                id="csharp"
+                name="csharp"/>    
+            <label htmlFor="csharp">C Sharp</label> 
+
+            <input 
+                type="checkbox" 
+                onChange={handleChange}
+                checked={formData.python} 
+                id="python"
+                name="python"/>    
+            <label htmlFor="python">Python</label> 
+
+            <input 
+                type="checkbox" 
+                onChange={handleChange}
+                checked={formData.javascript} 
+                id="javascript"
+                name="javascript"/>    
+            <label htmlFor="javascript">JavaScript</label> 
+        </div>
+    </div>          
     </form>
-  )
+  );
 }
 
-export default MyForm
+export default MyForm;
