e0acb90c6665d73661b43571beb67e457a35b59f
MishaelRatemo <ratemomishael@gmail.com>
2022-11-10 13:01:09 +0300

    form state as object--- combining values of many events as objects in the set state

--- src/Form.js
+++ src/Form.js
@@ -1,29 +1,41 @@
 import React, { useState } from 'react'
 
 function MyForm() {
-    const [firstName, setFirstName] = useState('')
-    const [lastName, setLastName] = useState('')
-
-    // console.log(firstName,lastName)
-
-    function handleFNChange(event){
-        setFirstName(event.target.value)
+    const [formdata, setFormData] = useState({
+        firstName:"", lastName:""
+    })
+    
+    function handleChange(event){
+        setFormData(prevFormData =>{
+            return{
+                ...prevFormData,
+                [event.target.name]:event.target.value
+            }
+        })
     }
-    function handleLNChange(event){
-        setLastName(event.target.value)
-    }
+
+    console.log(formdata)
+
+//     function handleFNChange(event){
+//         setFirstName(event.target.value)
+//     }
+//     function handleLNChange(event){
+//         setLastName(event.target.value)
+//     }
   return (
     <form>
         <h2>Sign Up</h2>
         <input 
             type="text"
             placeholder='First Name'
-            onChange={handleFNChange}        
+            onChange={handleChange}
+            name="firstName"        
         />
         <input 
             type="text"
             placeholder='Last Name'
-            onChange={handleLNChange}        
+            onChange={handleChange}
+            name="lastName"        
         />
 
     </form>
